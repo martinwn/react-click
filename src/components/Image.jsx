2c901d82e847e311199352ad0cac861b906b2504
unknown <martinwn@vcu.edu>
2018-09-18 15:01:33 -0400

    homepage feature

---
+++ src/components/Image.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Image extends Component {
+  render() {
+    const { onImageClick, show, image, score } = this.props;
+    const imageClasses = ["image"];
+    if (show && score === 0) {
+      imageClasses.push("show");
+    }
+    if (score > 0) {
+      imageClasses.push("make-visible");
+    }
+    return (
+      <img
+        src={this.props.image.srcImage}
+        onClick={() => onImageClick(image)}
+        className={imageClasses.join(" ")}
+        style={this.styles}
+      />
+    );
+  }
+}
+
+export default Image;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  state = {
+    message: ""
+  };
+
+  componentDidUpdate(prevProps) {
+    if (this.props.score > prevProps.score) {
+      this.setState({ message: "Correct!" });
+    } else if (this.props.score < prevProps.score) {
+      this.setState({ message: "Incorrect!" });
+    }
+  }
+
+  render() {
+    const { score, highScore } = this.props;
+    return (
+      <React.Fragment>
+        <nav className="site-header sticky-top py-1">
+          <div className="container d-flex flex-column flex-md-row justify-content-around">
+            <div>
+              <span id="score-keep">
+                Score: {score} | High Score: {highScore}
+              </span>
+            </div>
+            <div>
+              <span>{this.state.message || "Click to Start."}</span>
+            </div>
+          </div>
+        </nav>
+      </React.Fragment>
+    );
+  }
+}
+
+export default NavBar;
